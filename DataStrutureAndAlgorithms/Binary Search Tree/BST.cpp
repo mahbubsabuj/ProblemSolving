4a92cbdb4ef304f9c0c56373d328b4cc13f4e3f3
mahbubsabuj <mahbubsabuj3@gmail.com>
2022-07-25 17:55:09 +0600

    feat: :sparkles: Add Leetcode 173, 1008 and BST functions

--- DataStrutureAndAlgorithms/Binary Search Tree/BST.cpp
+++ DataStrutureAndAlgorithms/Binary Search Tree/BST.cpp
@@ -26,13 +26,7 @@ private:
 			root->left = insertBST(root->left, val);
 		}
 		return root;
-	}
-	void printInorder(TreeNode* root) {
-		if (root == nullptr) return;
-		printInorder(root->left);
-		cout << root->val << ' ';
-		printInorder(root->right);
-	}
+	};
 	TreeNode* findInorderSuccessor(TreeNode* root) {
 		TreeNode* current = root;
 		while (current != nullptr && current->left != nullptr) {
@@ -65,7 +59,48 @@ private:
 			}
 		}
 		return root;
- 	}
+ 	};
+	TreeNode* constructFromInorder(vector<int> &inorder, int l, int r) {
+		if (l > r) return nullptr;
+		int mid = l + (r - l) / 2;
+		TreeNode* root = new TreeNode(inorder[mid]);
+		root->left = constructFromInorder(inorder, l, mid - 1);
+		root->right = constructFromInorder(inorder, mid + 1, r);
+		return root;
+	};
+
+ 	TreeNode* constructFromPreorder(vector<int> &preorder, int* preorder_idx, int min, int max, int n) {
+		if (*preorder_idx == n) return nullptr;
+		TreeNode* root = nullptr;
+		int key = preorder[*preorder_idx];
+		if (key > min && key < max) {
+			root = new TreeNode(key);
+			*preorder_idx = *preorder_idx + 1;
+			if (*preorder_idx < n) {
+				root->left = constructFromPreorder(preorder, preorder_idx, min, key, n);
+			}
+			if (*preorder_idx < n) {
+				root->right = constructFromPreorder(preorder, preorder_idx, key, max, n);
+			}
+		}
+		return root;
+	};
+	TreeNode* constructFromPostorder(vector<int>& postorder, int* postorder_idx, int min, int max) {
+		if (*postorder_idx == -1) return nullptr;
+		TreeNode* root = nullptr;
+		int key = postorder[*postorder_idx];
+		if (key > min && key < max) {
+			root = new TreeNode(key);
+			*postorder_idx = *postorder_idx - 1;
+			if (*postorder_idx >= 0) {
+				root->right = constructFromPostorder(postorder, postorder_idx, key, max);
+			}
+			if (*postorder_idx >= 0) {
+				root->left = constructFromPostorder(postorder, postorder_idx, min, key);
+			}
+		}
+		return root;
+	}
 	bool isExist(TreeNode* root, int val) {
 		if (root == nullptr) return false;
 		if (root->val == val) {
@@ -76,8 +111,25 @@ private:
 		} else {
 			return isExist(root->right, val);
 		}
-	}
-	
+	};
+	void printInorder(TreeNode* root) {
+		if (root == nullptr) return;
+		printInorder(root->left);
+		cout << root->val << ' ';
+		printInorder(root->right);
+	};
+	void printPreorder(TreeNode* root) {
+		if (root == nullptr) return;
+		cout << root->val << ' ';
+		printPreorder(root->left);
+		printPreorder(root->right);
+	};
+	void printPostorder(TreeNode* root) {
+		if (root == nullptr) return;
+		printPostorder(root->left);
+		printPostorder(root->right);
+		cout << root->val << ' ';
+	};
 public:
 	void insertBST(int val) {
 		if (root == nullptr) {
@@ -86,31 +138,58 @@ public:
 			insertBST(root, val);
 		}
 	};
+	void constructFromInorder(vector<int> &inorder) {
+		root = constructFromInorder(inorder, 0, (int) inorder.size() - 1);
+	};
+	void constructFromPreorder(vector<int> &preorder) {
+		int preorder_idx = 0;
+		root = constructFromPreorder(preorder, &preorder_idx, INT_MIN, INT_MAX, (int) preorder.size());
+	};
+	void constructFromPostorder(vector<int> &postorder) {
+		int postorder_idx = (int) postorder.size() - 1;
+		root = constructFromPostorder(postorder, &postorder_idx, INT_MIN, INT_MAX);
+	};
 	void deleteFromBST(int val) {
 		deleteFromBST(root, val);
-	}
+	};
 	void printInorder() {
 		printInorder(root);
-	}
+	};
+	void printPreorder() {
+		printPreorder(root);
+	};
+	void printPostorder() {
+		printPostorder(root);
+	};
 	bool isExist(int val) {
 		return isExist(root, val);
-	}
+	};
 };
 
 
 int main () {
 	BST *bst = new BST();
-	bst->insertBST(10);
-	bst->insertBST(5);
-	bst->insertBST(3);
-	bst->insertBST(4);
-	bst->insertBST(6);
-	bst->insertBST(15);
-	bst->insertBST(18);
-	bst->printInorder();
-	cout << '\n';
-	bst->deleteFromBST(3);
-	bst->printInorder();
-	cout << '\n';
-
+	//~ bst->insertBST(10);
+	//~ bst->insertBST(5);
+	//~ bst->insertBST(3);
+	//~ bst->insertBST(4);
+	//~ bst->insertBST(6);
+	//~ bst->insertBST(15);
+	//~ bst->insertBST(18);
+	//~ bst->printInorder();
+	//~ cout << '\n';
+	//~ bst->deleteFromBST(3);
+	//~ bst->printInorder();
+	//~ cout << '\n';
+	// vector<int> preorder = {10, 2, 1, 13, 11};
+	// bst->constructFromPreorder(preorder);
+	// bst->printPreorder();
+	//~ vector<int> inorder = {1, 2, 10, 11, 13};
+	//~ bst->constructFromInorder(inorder);
+	//~ bst->printInorder();
+	//~ cout << '\n';
+	bst->printPostorder();
+	vector<int> postorder = {2, 1, 13, 11, 10};
+	bst->constructFromPostorder(postorder);
+	bst->printPostorder();
 }
